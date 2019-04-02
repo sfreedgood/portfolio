c4e806951566aa6aea59c9cef366604d9015269b
samfreedgood <sam.freedgood@gmail.com>
2019-04-01 22:36:47 -0400

    revert nav btn to grey

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -4,9 +4,9 @@ const NavBar = () => {
 
   return(
     <div className="default-primary-color nav-container">
-      <a className="accent-color nav-btn" href="#about">About</a>
-      <a className="accent-color nav-btn" href="#projects">Projects</a>
-      <a className="accent-color nav-btn" href="#contact">Contact</a>
+      <a className="nav-btn" href="#about">About</a>
+      <a className="nav-btn" href="#projects">Projects</a>
+      <a className="nav-btn" href="#contact">Contact</a>
     </div>
   )
 }
